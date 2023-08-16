c79f2d54703b3cb715f0ce7e3e3992f519bd2efb
devfabien <ish001fabi@gmail.com>
2023-08-16 11:18:41 +0200

    returning hello + client name

---
+++ GrasshopperDebug sayHello.ts
@@ -0,0 +1,11 @@
+//Grasshopper - Debug sayHello
+
+/* The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock */
+
+export function sayHello(name: string): string {
+    return `Hello, ${name}`
+  }
