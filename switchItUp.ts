c40af83a31f7a091e2c517a13f033ff56f666013
devfabien <ish001fabi@gmail.com>
2023-08-17 11:14:10 +0200

    function to return a text for anumber by using enum

--- switchItUp.ts
+++ switchItUp.ts
@@ -7,37 +7,42 @@ Output :: "One".
 
 If your language supports it, try using a switch statement. */
 
+// export function switchItUp(intNumber:number):string {
+//     switch(intNumber){
+//         case 1 :
+//         return 'One';
+//         break;
+//         case 2 :
+//         return 'Two';
+//         break;
+//         case 3 :
+//         return 'Three';
+//         break;
+//         case 4 :
+//         return 'Four';
+//         break;
+//         case 5 :
+//         return 'Five';
+//         break;
+//         case 6 :
+//         return 'Six';
+//         break;
+//         case 7 :
+//         return 'Seven';
+//         break;
+//         case 8 :
+//         return 'Eight';
+//         break;
+//         case 9 :
+//         return 'Nine';
+//         break;
+//         default:
+//         return 'Zero';
+//         break;
+//     }
+//   }
+
 export function switchItUp(intNumber:number):string {
-    switch(intNumber){
-        case 1 :
-        return 'One';
-        break;
-        case 2 :
-        return 'Two';
-        break;
-        case 3 :
-        return 'Three';
-        break;
-        case 4 :
-        return 'Four';
-        break;
-        case 5 :
-        return 'Five';
-        break;
-        case 6 :
-        return 'Six';
-        break;
-        case 7 :
-        return 'Seven';
-        break;
-        case 8 :
-        return 'Eight';
-        break;
-        case 9 :
-        return 'Nine';
-        break;
-        default:
-        return 'Zero';
-        break;
-    }
-  }
+    enum out{ Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten};
+    return out[intNumber];
+ }
