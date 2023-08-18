948f10d02fb56301bf0469ac972f54d5d10e07b3
devfabien <ish001fabi@gmail.com>
2023-08-18 11:06:38 +0200

    string ends with

---
+++ endsWith.ts
@@ -0,0 +1,10 @@
+//ends With
+/* Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false */
+export function solution(str: string, ending: string): boolean {
+    return str.endsWith(ending)
+  }
