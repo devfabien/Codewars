169f10170f30b1b277bb81e18f66b38e05de5ddf
devfabien <ish001fabi@gmail.com>
2023-08-21 10:36:21 +0200

    sort numbers

---
+++ sortNumbers.ts
@@ -0,0 +1,12 @@
+//Sort Numbers
+
+/* Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1, 2, 5, 10, 50]
+solution([]); // should return [] */
+
+export function solution(nums: number[]): number[] {
+    return nums.length>0?nums.sort((a,b)=>a-b):[]
+  }
