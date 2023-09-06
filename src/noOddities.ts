3443c466da7b9f87f1f264f17454b4849adcd0d6
devfabien <ish001fabi@gmail.com>
2023-09-06 10:26:18 +0200

    returning array that doesn't contain odd values

--- src/noOddities.ts
+++ src/noOddities.ts
@@ -3,3 +3,7 @@
 /* Write a small function that returns the values of an array that are not odd.
 
 All values in the array will be integers. Return the good values in the order they are given. */
+
+export function noOdds(values: number[]): number[] {
+    return values.filter(value=>value%2==0)
+  }
