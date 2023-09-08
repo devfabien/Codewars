81fce9b45706521c27f2b4003512d435d663734e
devfabien <ish001fabi@gmail.com>
2023-09-08 11:36:14 +0200

    Grasshopper - Function syntax debugging
    function solution

--- src/Functionsyntaxdebugging.ts
+++ src/Functionsyntaxdebugging.ts
@@ -1,6 +1,6 @@
 // Grasshopper - Function syntax debugging
 /* A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them. */
 
-export function main [verb: string, noun: string]: string
+export function main (verb: string, noun: string): string{
   return verb + noun
 }
