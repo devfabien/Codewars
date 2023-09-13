30669e9978c7f7511c1815e8d02b8e40473163e4
devfabien <ish001fabi@gmail.com>
2023-09-13 20:13:34 +0200

    Sum of angles
    adding the solution

--- src/sumOfAngles.ts
+++ src/sumOfAngles.ts
@@ -1,2 +1,6 @@
 //Sum of angles
 /* Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2. */
+
+export function angle(n: number): number {
+    return (n-2)*180
+  }
