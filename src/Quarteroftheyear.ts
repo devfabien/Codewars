4be417c5baa226ec1697d67363a6dc74c0681595
devfabien <ish001fabi@gmail.com>
2023-09-17 11:52:34 +0200

    Quarter of the year
    first solution using ternary operator

--- src/Quarteroftheyear.ts
+++ src/Quarteroftheyear.ts
@@ -7,3 +7,7 @@ For example: month 2 (February), is part of the first quarter; month 6 (June), i
 Constraint:
 
 1 <= month <= 12 */
+
+export function quarterOf(month: number): number {
+    return month<4?1:month<7?2:month<10?3:4
+  }
