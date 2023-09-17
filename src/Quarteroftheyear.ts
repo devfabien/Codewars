abbebcac97dda514ef1ef1735981da6e79b69b8c
devfabien <ish001fabi@gmail.com>
2023-09-17 11:53:17 +0200

    Quarter of the year
    second solution using ceil() method

--- src/Quarteroftheyear.ts
+++ src/Quarteroftheyear.ts
@@ -8,6 +8,10 @@ Constraint:
 
 1 <= month <= 12 */
 
+// export function quarterOf(month: number): number {
+//     return month<4?1:month<7?2:month<10?3:4
+//   }
+
 export function quarterOf(month: number): number {
-    return month<4?1:month<7?2:month<10?3:4
+    return Math.ceil(month/3)
   }
