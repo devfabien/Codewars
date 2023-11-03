c50e918fd9cf9b45e631e75915d15ef403f6a75c
devfabien <ish001fabi@gmail.com>
2023-11-03 17:33:18 +0200

    Is it palindrome adding the solution

--- src/isItPalindrome.ts
+++ src/isItPalindrome.ts
@@ -4,6 +4,7 @@
 
 A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar. */
 
+
 export function isPalindrome(x: string): boolean {
     return x.toLowerCase().split("").reverse().join("")==x.toLowerCase()
   }
